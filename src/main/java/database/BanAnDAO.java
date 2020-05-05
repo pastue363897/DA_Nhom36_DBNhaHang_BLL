c9b23fd5627c4c6cbd67d8e5c2e9ba9287d2b3fb
Ta Khanh Hoang <pastue363897@gmail.com>
2020-05-05 21:38:01 +0700

    [fix] sửa các hàm thêm bàn và món ăn để xử lý file [add] thêm hàm sửa bàn và món ăn

--- src/main/java/database/BanAnDAO.java
+++ src/main/java/database/BanAnDAO.java
@@ -7,55 +7,69 @@ package database;
 
 import javax.persistence.NoResultException;
 
+import org.apache.commons.io.FilenameUtils;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import entites.BanAn;
 
-public class BanAnDAO extends GeneralCRUD<BanAn>{
+public class BanAnDAO extends GeneralCRUD<BanAn> {
 
-  public BanAnDAO() {
-    super(BanAn.class);
-  }
-  
-  /**
-   * get last ID in alphanumeric order of BanAn
-   * @return the ID in string
-   */
-  private String getLastUsedID() {
-	  SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-	  Session session = sessionFactory.getCurrentSession();
-	  session.getTransaction().begin();
-	  String preparedQL = "select top 1 * from BanAn order by maBA desc";
-	  try {
-		  BanAn ba = (BanAn) session.createNativeQuery(preparedQL, BanAn.class).getSingleResult();
-		  session.getTransaction().commit();
-		  return ba.getMaBA();
-	  }
-	  catch (NoResultException ex)
-	  {
-		  session.getTransaction().commit();
-		  return "";
-	  }
-  }
-  
-  /**
-   * generate new ID for BanAn, it should be just take last used id then
-   * substring the number part out then add by one
-   * @return usable ID string for new object
-   */
-  private String generateID() {
-	  String lastUsed = getLastUsedID();
-	  if(lastUsed.equals("")) {
-		  return "BA000001";
-	  }
-	  String number = lastUsed.substring(2);
-	  return "BA"+String.format("%06d", Integer.parseInt(number)+1);
-  }
-  
-  public String addBanAn(BanAn banAn) {
-    banAn.setMaBA(generateID());
-    return this.save(banAn);
-  }
-  
+	public BanAnDAO() {
+		super(BanAn.class);
+	}
+
+	/**
+	 * get last ID in alphanumeric order of BanAn
+	 * 
+	 * @return the ID in string
+	 */
+	private String getLastUsedID() {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+		session.getTransaction().begin();
+		String preparedQL = "select top 1 * from BanAn order by maBA desc";
+		try {
+			BanAn ba = (BanAn) session.createNativeQuery(preparedQL, BanAn.class).getSingleResult();
+			session.getTransaction().commit();
+			return ba.getMaBA();
+		} catch (NoResultException ex) {
+			session.getTransaction().commit();
+			return "";
+		}
+	}
+
+	/**
+	 * generate new ID for BanAn, it should be just take last used id then substring
+	 * the number part out then add by one
+	 * 
+	 * @return usable ID string for new object
+	 */
+	private String generateID() {
+		String lastUsed = getLastUsedID();
+		if (lastUsed.equals("")) {
+			return "BA000001";
+		}
+		String number = lastUsed.substring(2);
+		return "BA" + String.format("%06d", Integer.parseInt(number) + 1);
+	}
+
+	public String addBanAn(BanAn banAn) {
+		banAn.setMaBA(generateID());
+		String tempPath = banAn.getHinhAnh();
+		String tempExtension = FilenameUtils.getExtension(tempPath);
+		String newPath = FilenameUtils.getPath(tempPath);
+		newPath += banAn.getMaBA() + "_table_image" + "." + tempExtension;
+		banAn.setHinhAnh(newPath);
+		return this.save(banAn);
+	}
+
+	public BanAn suaBanAn(BanAn banAn) {
+		String tempPath = banAn.getHinhAnh();
+		String tempExtension = FilenameUtils.getExtension(tempPath);
+		String newPath = FilenameUtils.getPath(tempPath);
+		newPath += banAn.getMaBA() + "_table_image" + "." + tempExtension;
+		banAn.setHinhAnh(newPath);
+		return this.update(banAn);
+	}
 }
--- src/main/java/database/MonAnDAO.java
+++ src/main/java/database/MonAnDAO.java
@@ -7,6 +7,7 @@ package database;
 
 import javax.persistence.NoResultException;
 
+import org.apache.commons.io.FilenameUtils;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -14,70 +15,87 @@ import org.hibernate.Transaction;
 import entites.CTTTBanDatMonAn;
 import entites.MonAn;
 
-public class MonAnDAO extends GeneralCRUD<MonAn>{
+public class MonAnDAO extends GeneralCRUD<MonAn> {
 
-  private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-  public MonAnDAO() {
-    super(MonAn.class);
-  }
-  /**
-   * get last ID in alphanumeric order of MonAn
-   * @return the ID in string
-   */
-  private String getLastUsedID() {
-    Session session = sessionFactory.getCurrentSession();
-    session.getTransaction().begin();
-    String preparedQL = "select top 1 * from MonAn order by maMA desc";
-    try {
-      MonAn ma = (MonAn) session.createNativeQuery(preparedQL, MonAn.class).getSingleResult();
-      session.getTransaction().commit();
-      return ma.getMaMA();
-    }
-    catch (NoResultException ex)
-    {
-      session.getTransaction().commit();
-      return "";
-    }
-  }
-  
-  /**
-   * generate new ID for MonAn, it should be just take last used id then
-   * substring the number part out then add by one
-   * @return usable ID string for new object
-   */
-  private String generateID() {
-    String lastUsed = getLastUsedID();
-    if(lastUsed.equals("")) {
-      return "MA000001";
-    }
-    String number = lastUsed.substring(2);
-    return "MA"+String.format("%06d", Integer.parseInt(number)+1);
-  }
-  
-  public String addMonAn(MonAn monAn) {
-    monAn.setMaMA(generateID());
-    return this.save(monAn);
-  }
-  
-  public boolean checkPreviouslyBooked(MonAn monAn) {
-    Session session = sessionFactory.getCurrentSession();
-    Transaction tr = session.getTransaction();
-    try {
-      tr.begin();
-      String sql = "select top 1 * from CTTTBanDatMonAn where monAn = '" + monAn.getMaMA() + "'";
-      CTTTBanDatMonAn ct = session.createNativeQuery(sql, CTTTBanDatMonAn.class).getSingleResult();
-      tr.commit();
-      if (ct != null) {
-        return true;
-      }
-    } catch (Exception e) {
-      tr.rollback();
-      e.printStackTrace();
-    }
-    return false;
-  }
-  public boolean setIsDeleted(MonAn monAn) {
-    monAn.setDeleted(true);
-    return this.saveOrUpdate(monAn);
-  }
+	private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+	public MonAnDAO() {
+		super(MonAn.class);
+	}
+
+	/**
+	 * get last ID in alphanumeric order of MonAn
+	 * 
+	 * @return the ID in string
+	 */
+	private String getLastUsedID() {
+		Session session = sessionFactory.getCurrentSession();
+		session.getTransaction().begin();
+		String preparedQL = "select top 1 * from MonAn order by maMA desc";
+		try {
+			MonAn ma = (MonAn) session.createNativeQuery(preparedQL, MonAn.class).getSingleResult();
+			session.getTransaction().commit();
+			return ma.getMaMA();
+		} catch (NoResultException ex) {
+			session.getTransaction().commit();
+			return "";
+		}
+	}
+
+	/**
+	 * generate new ID for MonAn, it should be just take last used id then substring
+	 * the number part out then add by one
+	 * 
+	 * @return usable ID string for new object
+	 */
+	private String generateID() {
+		String lastUsed = getLastUsedID();
+		if (lastUsed.equals("")) {
+			return "MA000001";
+		}
+		String number = lastUsed.substring(2);
+		return "MA" + String.format("%06d", Integer.parseInt(number) + 1);
+	}
+
+	public String addMonAn(MonAn monAn) {
+		monAn.setMaMA(generateID());
+		String tempPath = monAn.getHinhAnhMA();
+		String tempExtension = FilenameUtils.getExtension(tempPath);
+		String newPath = FilenameUtils.getPath(tempPath);
+		newPath += monAn.getMaMA() + "_meal_image" + "." + tempExtension;
+		monAn.setHinhAnhMA(newPath);
+		return this.save(monAn);
+	}
+
+	public MonAn suaMonAn(MonAn monAn) {
+		String tempPath = monAn.getHinhAnhMA();
+		String tempExtension = FilenameUtils.getExtension(tempPath);
+		String newPath = FilenameUtils.getPath(tempPath);
+		newPath += monAn.getMaMA() + "_meal_image" + "." + tempExtension;
+		monAn.setHinhAnhMA(newPath);
+		return this.update(monAn);
+	}
+
+	public boolean checkPreviouslyBooked(MonAn monAn) {
+		Session session = sessionFactory.getCurrentSession();
+		Transaction tr = session.getTransaction();
+		try {
+			tr.begin();
+			String sql = "select top 1 * from CTTTBanDatMonAn where monAn = '" + monAn.getMaMA() + "'";
+			CTTTBanDatMonAn ct = session.createNativeQuery(sql, CTTTBanDatMonAn.class).getSingleResult();
+			tr.commit();
+			if (ct != null) {
+				return true;
+			}
+		} catch (Exception e) {
+			tr.rollback();
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public boolean setIsDeleted(MonAn monAn) {
+		monAn.setDeleted(true);
+		return this.saveOrUpdate(monAn);
+	}
 }
