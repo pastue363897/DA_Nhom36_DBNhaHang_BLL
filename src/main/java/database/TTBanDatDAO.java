8d9e2ca7a14a63954cdc80f88b1b0eb13274e156
Dinh Van Dung <dinhvandungyknb6789@gmail.com>
2020-05-13 23:45:48 +0700

    [refactor] cập nhật function tổng tiền bàn đặt

--- src/main/java/database/TTBanDatDAO.java
+++ src/main/java/database/TTBanDatDAO.java
@@ -6,6 +6,7 @@
 package database;
 
 import javax.persistence.NoResultException;
+import javax.persistence.Query;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -59,6 +60,24 @@ public class TTBanDatDAO extends GeneralCRUD<TTBanDat> {
   public String addBanDat(TTBanDat ttBD) {
     ttBD.setMaBD(generateID());
     return this.save(ttBD);
+  }
+  public boolean updateTongTien(String maBD, long tongTien) {
+    Session session = sessionFactory.getCurrentSession();
+    session.getTransaction().begin();
+    boolean result = false;
+    String sql = "update TTBanDat set tongTien = :tongTien where maBD = :maBD";
+    Query query = session.createQuery(sql);
+    query.setParameter("tongTien", tongTien);
+    query.setParameter("maBD", maBD);
+    try {
+      if (query.executeUpdate() > 0) {
+        result = true;
+      }
+      session.getTransaction().commit();
+    } catch (NoResultException ex) {
+      session.getTransaction().rollback();
+    }
+    return result;
   }
 
 }
